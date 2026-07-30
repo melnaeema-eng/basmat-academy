47bbe06bb063a89a7b1c49e6817d0c1a7c6830fc
Magdi Elnaeem <melnaeema@gmail.com>
2026-07-30 14:19:47 +0300

    Describe the completed feature

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,10 +5,12 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Courses from "../pages/Courses";
+import CourseDetails from "../pages/CourseDetails";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
 import ProtectedRoute from "../components/ProtectedRoute";
+
 // Admin
 import Dashboard from "../pages/admin/Dashboard";
 import AddCourse from "../pages/admin/AddCourse";
@@ -22,32 +24,63 @@ export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-
         {/* صفحات الموقع */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/courses" element={<Courses />} />
+
+        <Route
+          path="/courses/:id"
+          element={<CourseDetails />}
+        />
+
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
 
-        {/* صفحات الإدارة */}
+        {/* تسجيل دخول الإدارة */}
         <Route
-  path="/admin"
-  element={
-    <ProtectedRoute>
-      <AdminLayout />
-    </ProtectedRoute>
-  }
->
-  <Route path="dashboard" element={<Dashboard />} />
-  <Route path="courses" element={<AdminCourses />} />
-  <Route path="add-course" element={<AddCourse />} />
-  <Route path="view-course/:id" element={<ViewCourse />} />
-  <Route path="edit-course/:id" element={<EditCourse />} />
-</Route>
+          path="/admin/login"
+          element={<AdminLogin />}
+        />
 
-<Route path="/admin/login" element={<AdminLogin />} />
+        {/* صفحات الإدارة المحمية */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route
+            path="dashboard"
+            element={<Dashboard />}
+          />
+
+          <Route
+            path="courses"
+            element={<AdminCourses />}
+          />
+
+          <Route
+            path="add-course"
+            element={<AddCourse />}
+          />
+
+          <Route
+            path="view-course/:id"
+            element={<ViewCourse />}
+          />
+
+          <Route
+            path="edit-course/:id"
+            element={<EditCourse />}
+          />
+        </Route>
+
+        {/* أي رابط غير موجود */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
